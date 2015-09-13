49ff4e8d5992281f361edb79351217ba8e5a58e9
yippee-ki-yay <nesa993@gmail.com>
2015-09-13 19:31:17 +0200

    first page setup on github

---
+++ game.js
@@ -0,0 +1,53 @@
+function game()
+{
+	"use strict";
+	
+	var WIDTH = 600;
+	var HEIGHT = 800;
+	var FPS = 25;
+		
+	var canvas = null;
+		
+	(function main()
+	{
+		init();
+	})();
+	
+	function init()
+	{
+		var canvasElem = $("<canvas width='" + WIDTH + "' height='" + HEIGHT + "'></canvas>");
+		canvas = canvasElem.get(0).getContext("2d");
+	    
+		$("#game_canvas").append(canvasElem);
+		
+		if(annyang)
+		{
+			var commands = {"jump" : function(){alert("jump");}};
+			
+			annyang.addCommands(commands);
+			annyang.start();
+		}
+		
+	}
+	
+	function gameLoop()
+	{
+		draw();
+		update();
+	}
+	
+	function draw()
+	{
+		canvas.fillStyle = "#000";
+		canvas.fillText("what up dog", 100, 100);
+	}
+	
+	function update()
+	{
+		
+	}
+	
+	//call the game loop FPS(25) time each second
+	setInterval(gameLoop, 1000/FPS);
+	
+};
